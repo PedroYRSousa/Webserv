3eca6685b77f3ba559f5905ef7a0192a33cf980b
pyago-ra <yagosousa2512@gmail.com>
2023-08-27 12:17:28 -0300

    Update: Exemplo de testes feito

---
+++ src/Sum.cpp
@@ -0,0 +1,16 @@
+#include "Sum.hpp"
+
+int sum(int a, int b)
+{
+  return a + b;
+}
+
+#ifdef TEST_MODE
+#include <gtest/gtest.h>
+
+TEST(SumTest, DemonstrandoOsTestesUnitários)
+{
+  EXPECT_EQ(5, sum(3, 2)); // Sucesso
+  EXPECT_EQ(5, sum(2, 2)); // Falha
+}
+#endif // TEST_MODE
