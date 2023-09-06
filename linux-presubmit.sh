899e0fb50ca6b83ebac73e90f746709c59d0e165
pyago-ra <yagosousa2512@gmail.com>
2023-09-06 09:33:47 -0300

    CI: tests

--- linux-presubmit.sh
+++ linux-presubmit.sh
@@ -1,16 +1,9 @@
 set -euox pipefail
-
-readonly LINUX_LATEST_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20230816"
-
+readonly LINUX_LATEST_CONTAINER="debian:latest"
 GTEST_ROOT="$(realpath $(dirname ${0})/..)"
-# Defina a pasta de trabalho dentro do container
 DIR=$(pwd)
 WORKDIR="/src"
-
-# Comando para compilar e executar o projeto
 COMMAND="git clone https://github.com/google/googletest.git && cd googletest && mkdir -p build && cd build && cmake .. && make && make install && cd .. && cd .. && rm -rf googletest && make test && ./webserv_unit_test"
-
-# Execute o comando dentro do container
 docker run \
   --volume="${GTEST_ROOT}:${GTEST_ROOT}" \
   --workdir="${DIR}" \
