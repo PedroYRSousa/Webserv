d806d5fa131cf9cc4114bc44bbb36eb6347f2d60
pyago-ra <yagosousa2512@gmail.com>
2023-09-06 02:11:04 -0300

    CI: tests

--- linux-presubmit.sh
+++ linux-presubmit.sh
@@ -3,16 +3,11 @@ set -euox pipefail
 readonly LINUX_LATEST_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20230816"
 
 GTEST_ROOT="$(realpath $(dirname ${0})/..)"
-# Defina a pasta de trabalho dentro do container
-DIR=$(pwd)
-WORKDIR="/src"
 
-# Comando para compilar e executar o projeto
 COMMAND="make test && ./webserv_unit_test"
 
-# Execute o comando dentro do container
 docker run \
   --volume="${GTEST_ROOT}:${GTEST_ROOT}" \
-  --workdir="${DIR}" \
+  --workdir="${GTEST_ROOT}" \
   ${LINUX_LATEST_CONTAINER} \
   bash -c "${COMMAND}"
