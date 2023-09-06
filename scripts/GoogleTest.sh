1a35408e3585864f5c95cff11d839e826abb5e7f
pyago-ra <yagosousa2512@gmail.com>
2023-09-06 12:51:14 -0300

    CI: Fluxo finalizado

--- scripts/GoogleTest.sh
+++ scripts/GoogleTest.sh
@@ -3,7 +3,7 @@ readonly LINUX_LATEST_CONTAINER="gcr.io/google.com/absl-177019/linux_gcc-latest:
 GTEST_ROOT="$(realpath $(dirname ${0})/..)"
 DIR=$(pwd)
 WORKDIR="/src"
-COMMAND="git clone https://github.com/google/googletest.git && cd googletest && mkdir -p build && cd build && cmake .. && make && make install && cd .. && cd .. && rm -rf googletest && make test && ./webserv_unit_test"
+COMMAND="git clone https://github.com/google/googletest.git && cd googletest && mkdir -p build && cd build && cmake .. && make && make install && cd .. && cd .. && rm -rf googletest && make googletest"
 docker run \
   --volume="${GTEST_ROOT}:${GTEST_ROOT}" \
   --workdir="${DIR}" \
