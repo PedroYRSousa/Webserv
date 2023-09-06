ccd88d244473ef2e5907c68d2de9b6bd7e2ee030
pyago-ra <yagosousa2512@gmail.com>
2023-09-06 02:03:23 -0300

    CI: tests

--- linux-presubmit.sh
+++ linux-presubmit.sh
@@ -1,97 +1,22 @@
-#!/bin/bash
-#
-# Copyright 2020, Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 set -euox pipefail
 
 readonly LINUX_LATEST_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20230816"
-readonly LINUX_GCC_FLOOR_CONTAINER="gcr.io/google.com/absl-177019/linux_gcc-floor:20230120"
 
 if [[ -z ${GTEST_ROOT:-} ]]; then
   GTEST_ROOT="$(realpath $(dirname ${0})/..)"
 fi
 
-if [[ -z ${STD:-} ]]; then
-  STD="c++14 c++17 c++20"
-fi
+# Defina a pasta de trabalho dentro do container
+WORKDIR="/src"
 
-echo ${GTEST_ROOT}
+# Comando para compilar e executar o projeto
+COMMAND="make test && ./seu_binario"
 
-# Test GCC
-for std in ${STD}; do
-  for absl in 0 1; do
-    echo "2"
-    time docker run \
-      --volume="${GTEST_ROOT}:/src:ro" \
-      --workdir="/src" \
-      --rm \
-      --env="CC=/usr/local/bin/gcc" \
-      --env="BAZEL_CXXOPTS=-std=${std}" \
-      ${LINUX_LATEST_CONTAINER} \
-      /usr/local/bin/bazel test ... \
-        --copt="-Wall" \
-        --copt="-Werror" \
-        --copt="-Wuninitialized" \
-        --copt="-Wundef" \
-        --define="absl=${absl}" \
-        --distdir="/bazel-distdir" \
-        --features=external_include_paths \
-        --keep_going \
-        --show_timestamps \
-        --test_output=errors
-  done
-done
-
-## Test Clang
-#for std in ${STD}; do
-#  for absl in 0 1; do
-#    echo "3"
-#    time docker run \
-#      --volume="${GTEST_ROOT}:/src:ro" \
-#      --workdir="/src" \
-#      --rm \
-#      --env="CC=/opt/llvm/clang/bin/clang" \
-#      --env="BAZEL_CXXOPTS=-std=${std}" \
-#      ${LINUX_LATEST_CONTAINER} \
-#      /usr/local/bin/bazel test ... \
-#        --copt="--gcc-toolchain=/usr/local" \
-#        --copt="-Wall" \
-#        --copt="-Werror" \
-#        --copt="-Wuninitialized" \
-#        --copt="-Wundef" \
-#        --define="absl=${absl}" \
-#        --distdir="/bazel-distdir" \
-#        --features=external_include_paths \
-#        --keep_going \
-#        --linkopt="--gcc-toolchain=/usr/local" \
-#        --show_timestamps \
-#        --test_output=errors
-#  done
-#done
+# Execute o comando dentro do container
+docker run \
+  --volume="${GTEST_ROOT}:${WORKDIR}:ro" \
+  --workdir="${WORKDIR}" \
+  --rm \
+  --env="CC=/usr/local/bin/gcc" \
+  ${LINUX_LATEST_CONTAINER} \
+  bash -c "${COMMAND}"
