555e297761e85c79bd63ec8b17633beb89df5e62
pyago-ra <yagosousa2512@gmail.com>
2023-09-10 14:02:55 -0300

    Update: getPoll criado

--- includes/A_Connection.hpp
+++ includes/A_Connection.hpp
@@ -1,11 +1,22 @@
 #ifndef A_CONNECTION_HPP
 #define A_CONNECTION_HPP
 
+#include "Error.hpp"
+
+#include <vector>
+#include <poll.h>
+
 class A_Connection
 {
 public:
+	pollfd &getPoll();
+	virtual Error init(void) = 0;
+
 protected:
+	static std::vector<pollfd *> polls;
+
 private:
+	pollfd poll;
 };
 
 #endif // A_CONNECTION_HPP
---
+++ src/A_Connection.cpp
@@ -0,0 +1,13 @@
+#include "A_Connection.hpp"
+
+// STATIC LOCAL
+// STATIC PUBLIC
+// STATIC PROTECTED
+// STATIC PRIVATE
+// PUBLIC
+pollfd &A_Connection::getPoll()
+{
+	return this->poll;
+}
+// PROTECTED
+// PRIVATE
