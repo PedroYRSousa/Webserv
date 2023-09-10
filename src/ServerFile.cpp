b886a765857adca8ffe6932625236842f136a70f
pyago-ra <yagosousa2512@gmail.com>
2023-09-10 14:10:39 -0300

    Update: ServerFile constructor criado

---
+++ src/ServerFile.cpp
@@ -0,0 +1,15 @@
+#include "ServerFile.hpp"
+
+// STATIC LOCAL
+// STATIC PUBLIC
+// STATIC PROTECTED
+// STATIC PRIVATE
+// PUBLIC
+ServerFile::ServerFile(int port) : A_Server(port)
+{
+}
+ServerFile::~ServerFile()
+{
+}
+// PROTECTED
+// PRIVATE
